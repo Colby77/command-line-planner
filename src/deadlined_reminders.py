b6fa1bb23b52773fc9a8d49157c25649b0911b7a
Colby Workman <cgw2319@gmail.com>
2021-12-15 16:46:53 -0700

    initial commit

---
+++ src/deadlined_reminders.py
@@ -0,0 +1,47 @@
+from abc import ABCMeta, abstractmethod, ABC
+from collections.abc import Iterable
+from datetime import datetime
+from dateutil.parser import parse
+
+class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
+
+    @abstractmethod
+    def is_due(self):
+        pass
+
+
+class DeadlinedReminder(ABC, Iterable):
+
+    @abstractmethod
+    def is_due(self):
+        pass
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        if cls is not DeadlinedReminder:
+            return NotImplemented
+
+        def attr_in_hierarchy(attr):
+            return any (attr in SuperClass.__dict__ for SuperClass in subclass.__mro__)
+
+        if not all(attr_in_hierarchy(attr) for attr in ('__iter__', 'is_due')):
+            return NotImplemented
+
+        return True
+
+class DateReminder(DeadlinedReminder):
+
+    def __init__(self, text, date):
+        self.text = text
+        self.date = parse(date, dayfirst=True)
+
+    def is_due(self):
+        if self.date <= datetime.now():
+            return True
+        else:
+            return False
+
+    def __iter__(self):
+        # formatted_date = self.date.isoformat()
+        # return iter([self.text, formatted_date])
+        return iter([self.text, self.date.isoformat()])
